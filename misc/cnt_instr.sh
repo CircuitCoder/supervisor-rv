2260bfda46f2527e4d28de2a8c0baf9ef02e64dd
Liu Xiaoyi <circuitcoder0@gmail.com>
2020-11-22 20:33:44 +0800

    Added howto for cnt_instr.sh

--- misc/cnt_instr.sh
+++ misc/cnt_instr.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# How to use: Within /kernel
+# $ riscv64-unknown-elf-objdump -S -Mno-aliases kernel.elf | ../misc/cnt_instr.sh
+
+
 cat - \
   | grep -oE "^[0-9A-Fa-f]+:\s*[0-9A-Fa-f]+\s*(\S+)(\s|$)" \
   | awk '{ print $3 }' \
